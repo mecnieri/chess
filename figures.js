423cf2a9f7362c1c4c1903622af2ed2a8527f33a
kripersa <100306879+kripersa@users.noreply.github.com>
2023-05-21 00:53:10 +0400

    fiiilll boox

--- figures.js
+++ figures.js
@@ -1,4 +1,14 @@
 export let figures = {
-  WPawn: '<i class="fa-sharp fa-regular fa-chess-pawn"></i>',
   BPawn: '<i class="fa-solid fa-chess-pawn"></i>',
-}
+  WPawn: '<i class="fa-sharp fa-regular fa-chess-pawn"></i>',
+  BRook: '<i class="fa-solid fa-chess-rook"></i>',
+  WRook: '<i class="fa-light fa-chess-rook"></i>',
+  BKnight: '<i class="fa-solid fa-chess-knight"></i>',
+  WKnight: '<i class="fa-regular fa-chess-knight"></i>',
+  BBishop: '<i class="fa-solid fa-chess-bishop"></i>',
+  WBishop: '<i class="fa-light fa-chess-bishop"></i>',
+  BQueen: '<i class="fa-solid fa-chess-queen"></i>',
+  WQueen: '<i class="fa-regular fa-chess-queen"></i>',
+  BKing: '<i class="fa-solid fa-chess-king"></i>',
+  WKing: '<i class="fa-light fa-chess-king"></i>',
+};
